54a8c9c3970bfeb327c3805c784b3747d38421c1
Tomas <tomasloon@gmail.com>
2011-12-21 13:33:40 -0200

    Less lines!

--- MVC/1/FakeAjaxCall.js
+++ MVC/1/FakeAjaxCall.js
@@ -1,63 +1,6 @@
 jQuery.ajax = function(oConfig){
 	switch(oConfig.url){
-		case "/todo-items/add":
-			oConfig.success([
-				{
-					text: 'Test 1',
-					done: false,
-					id: 1
-				},
-				{
-					text: 'Test 2',
-					done: false,
-					id: 2
-				},
-				{
-					text: 'Test 3',
-					done: true,
-					id: 3
-				}
-			]);
-			break;
-		case "/todo-items":
-			oConfig.success([
-				{
-					text: 'Test 1',
-					done: false,
-					id: 1
-				},
-				{
-					text: 'Test 2',
-					done: false,
-					id: 2
-				},
-				{
-					text: 'Test 3',
-					done: true,
-					id: 3
-				}
-			]);
-			break;
-		case "/todo-items/undo":
-			oConfig.success([
-				{
-					text: 'Test 1',
-					done: false,
-					id: 1
-				},
-				{
-					text: 'Test 2',
-					done: false,
-					id: 2
-				},
-				{
-					text: 'Test 3',
-					done: false,
-					id: 3
-				}
-			]);
-			break;
-		case "/todo-items/complete":
+		default:
 			oConfig.success([
 				{
 					text: 'Test 1',
